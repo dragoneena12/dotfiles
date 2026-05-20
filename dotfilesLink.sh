763ad93c4acead53e2149ddb3db7c9286fd8cdcd
lapi <tachibana_a@tokium.jp>
2026-05-20 10:03:50 +0900

    add aerospace

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -p ~/.config/sheldon
+mkdir -p ~/.config/aerospace
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.textlintrc ~/.textlintrc
 ln -sf ~/dotfiles/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
+ln -sf ~/dotfiles/aerospace.toml ~/.config/aerospace/aerospace.toml
